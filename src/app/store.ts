f697f885945eb261d0fe75df8f9d647dfac28f54
Ak <akalankapathirage22@gmail.com>
2022-01-07 12:20:16 +0530

    removed counter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import productReducer from '../features/shopSlice'
-import counterReducer from '../features/counter/counterSlice';
 
 
 export const store = configureStore({
   reducer: {
     products : productReducer,
-    counter: counterReducer,
   },
 });
 
